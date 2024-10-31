d3e5a660b34327b5680d1479997e890dbeb9db8e
Marwa_100 <marwa.emam100@gmail.com>
2024-10-31 10:48:06 +0300

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,36 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/views/edit_notes_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16)
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text('Flutter Tips',style: TextStyle(fontSize: 26,color: Colors.black),),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text('Build your career with Marwa Emam',
-              style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5)),),
+    return  GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder:(context){return EditNotesView();} ));
+      },
+      child: Container(
+        padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text('Flutter Tips',style: TextStyle(fontSize: 26,color: Colors.black),),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Build your career with Marwa Emam',
+                style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5)),),
+              ),
+              trailing: Icon(FontAwesomeIcons.trash,size: 24,color: Colors.black,),
+        
             ),
-            trailing: Icon(FontAwesomeIcons.trash,size: 24,color: Colors.black,),
-  
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24,top: 16),
-            child: Text('May21 ,2024',
-            style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5)),),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 24,top: 16),
+              child: Text('May21 ,2024',
+              style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5)),),
+            )
+          ],
+        ),
       ),
     );
   }
