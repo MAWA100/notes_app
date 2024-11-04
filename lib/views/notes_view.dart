0915b53a3532b53cbfdd0f3b4ef040d963c9d1e9
Marwa_100 <marwa.emam100@gmail.com>
2024-11-04 12:16:12 +0200

    create fetch notes method

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/widgets/add_modal_bottom_sheet.dart';
 import 'package:note_app/widgets/notes_view_body.dart';
 
@@ -7,21 +9,24 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          showModalBottomSheet(context: context, builder: (context){
-            
-            return const AddModalBottomSheet();
-          },
-          isScrollControlled: true
-          ,shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16)
-          ));
-        },shape: CircleBorder(),
-        
-        child:const Icon(Icons.add),),
-      body: const NotesViewBody()
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return const AddModalBottomSheet();
+                  },
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)));
+            },
+            shape: CircleBorder(),
+            child: const Icon(Icons.add),
+          ),
+          body: const NotesViewBody()),
     );
   }
 }
