1503274ef17a029e743d7e98ecc722014ce1adff
Marwa_100 <marwa.emam100@gmail.com>
2024-11-06 11:16:18 +0200

    display notes part two

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/edit_notes_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +16,17 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
         decoration: BoxDecoration(
-          color: Color(0xffFFCC80),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16)
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: Text('Flutter Tips',style: TextStyle(fontSize: 26,color: Colors.black),),
+              title: Text(note.title,style: TextStyle(fontSize: 26,color: Colors.black),),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 16),
-                child: Text('Build your career with Marwa Emam',
+                child: Text(note.subTitle,
                 style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5)),),
               ),
               trailing: Icon(FontAwesomeIcons.trash,size: 24,color: Colors.black,),
@@ -32,7 +34,7 @@ class NoteItem extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(right: 24,top: 16),
-              child: Text('May21 ,2024',
+              child: Text(note.date,
               style: TextStyle(fontSize: 18,color: Colors.black.withOpacity(.5)),),
             )
           ],
